1be712db0570f9f97e872439494255c63ef833f5
FrancoARossi <rossifrancoa@gmail.com>
2025-05-03 18:40:18 -0300

    chore: eslint configs

--- eslint.config.js
+++ eslint.config.js
@@ -32,6 +32,14 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'react/react-in-jsx-scope': 'off',
+      'jsx-a11y/anchor-is-valid': 'off',
+      '@typescript-eslint/no-unused-vars': ['error'],
+      '@typescript-eslint/explicit-function-return-type': ['off'],
+      '@typescript-eslint/explicit-module-boundary-types': ['off'],
+      '@typescript-eslint/no-empty-function': ['off'],
+      '@typescript-eslint/no-explicit-any': ['off'],
+      'prettier/prettier': ['error', {}, { usePrettierrc: true }],
       'check-file/filename-naming-convention': [
         'error',
         {
