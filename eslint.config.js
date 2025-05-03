f271c9c300d542716f59c904ca9f2a218b3da1ed
FrancoARossi <rossifrancoa@gmail.com>
2025-05-03 15:12:50 -0300

    chore: folder structure & dependencies init

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,12 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettierPlugin from 'eslint-plugin-prettier';
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
+import checkFilePlugin from 'eslint-plugin-check-file';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -16,6 +20,10 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      react: reactPlugin,
+      prettier: prettierPlugin,
+      'jsx-a11y': jsxA11yPlugin,
+      'check-file': checkFilePlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +31,23 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'check-file/filename-naming-convention': [
+        'error',
+        {
+          '**/*.{ts,tsx}': 'KEBAB_CASE',
+        },
+        {
+          // ignore the middle extensions of the filename to support filename like bable.config.js or smoke.spec.ts
+          ignoreMiddleExtensions: true,
+        },
+      ],
+      'check-file/folder-naming-convention': [
+        'error',
+        {
+          // all folders within src (except __tests__)should be named in kebab-case
+          'src/**/!(__tests__)': 'KEBAB_CASE',
+        },
+      ],
     },
   },
-)
+);
---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <p>This is a simple React app.</p>
+    </div>
+  );
+};
+
+export default App;
