183d0ee19fe34b6f1de2e0dbd1c9dce5646a1669
FrancoARossi <rossifrancoa@gmail.com>
2025-06-08 21:25:16 -0300

    [SHE-26] sign in link auth state

--- src/features/auth/components/sign-in-link.tsx
+++ src/features/auth/components/sign-in-link.tsx
@@ -1,14 +1,17 @@
 import { Text } from '@/components/ui/text';
+import { useAuth } from 'react-oidc-context';
 import { NavLink } from 'react-router';
 
 export const SignInLink = () => {
+  const { isAuthenticated } = useAuth();
+
   return (
     <NavLink
-      to="/auth/sign-in"
+      to={isAuthenticated ? '/app/pets' : '/auth/sign-in'}
       className="rounded-lg bg-amber-200 px-4 py-2 transition-colors hover:bg-amber-300"
     >
       <Text element="h2" className="text-md font-semibold">
-        Iniciar sesión
+        {isAuthenticated ? 'Acceder' : 'Iniciar sesión'}
       </Text>
     </NavLink>
   );
